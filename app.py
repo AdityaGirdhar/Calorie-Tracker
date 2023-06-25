d79f778d666879cf21ac4c9c29b52025490592c2
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-25 21:39:18 +0530

    Added filter capabilities and pagination support

--- app.py
+++ app.py
@@ -9,6 +9,28 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+def get_paginated_filtered_records(user_id, date=None, text=None, calories_min=None, calories_max=None, page=None, limit=None):
+	base_query = Entry.query.filter_by(user_id=user_id)
+	if date:
+		base_query = base_query.filter(Entry.date == date)
+	if text:
+		base_query = base_query.filter(Entry.text.ilike(f"%{text}%"))
+	if calories_min:
+		base_query = base_query.filter(Entry.calories >= calories_min)
+	if calories_max:
+		base_query = base_query.filter(Entry.calories <= calories_max)
+
+	# Calculate the offset and limit for pagination
+	offset = ((page - 1) * limit) if (page != None and limit != None) else None
+	# Apply pagination to the query
+	records_query = base_query.offset(offset).limit(limit)
+	# Retrieve the records from the database
+	records = records_query.all()
+	# Get the total count of records for pagination
+	total_count = base_query.count()
+ 
+	return records, total_count
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -92,14 +114,54 @@ def signup():
 @app.route('/users', methods=['GET'])
 @login_required
 def get_users():
-	user = current_user
-	# Only allow access if user's role is manager or admin
-	if (user.role == 'manager' or user.role == 'admin'):
-		all_users = User.query.all()
+	try:
+		data = request.get_json()
+	except:
+		data = {}
+  
+	# Get the query parameters for filtering
+	username = data['username'] if 'username' in data else None
+	user_id = data['user_id'] if 'user_id' in data else None
+	print(user_id)
+	role = data['role'] if 'role' in data else None
+	base_query = User.query
+
+	if user_id:
+		base_query = base_query.filter(User.id == user_id)
+	if username:
+		base_query = base_query.filter(User.username == username)
+	if role:
+		base_query = base_query.filter(User.role == role)
+  
+	try:
+		page = int(data['page']) if 'page' in data else None
+		limit = int(data['limit']) if 'limit' in data else None
+	except:
+		abort(400)
+
+	# Calculate the offset and limit for pagination
+	offset = ((page - 1) * limit) if (page != None and limit != None) else None
+	# Apply pagination to the query
+	records_query = base_query.offset(offset).limit(limit)
+	# Retrieve the records from the database
+	records = records_query.all()
+	# Get the total count of records for pagination
+	total_count = base_query.count()
+ 
+	if role:
+		if (current_user.role not in { 'manager', 'admin' }):
+			abort(403)
 		res = { 'users' : [], 'managers' : [], 'admins' : [] }
-		for user in all_users:
-			res[user.role + 's'].append({'username': user.username, 'expected_calories': user.expected_calories})
-		return jsonify(res)
+		for user in records:
+			res[user.role + 's'].append({ 'user_id': user.id, 'username': user.username, 'expected_calories': user.expected_calories})
+		return jsonify({ 'users': res, 'total_count': total_count, 'page': page, 'limit': limit })
+
+	# Only allow access if user's role is manager or admin
+	if (current_user.role == 'manager' or current_user.role == 'admin'):
+		res = { 'users' : [], 'managers' : [], 'admins' : [] }
+		for user in records:
+			res[user.role + 's'].append({ 'user_id': user.id, 'username': user.username, 'expected_calories': user.expected_calories})
+		return jsonify({ 'users': res, 'total_count': total_count, 'page': page, 'limit': limit })
 	else:
 		abort(403)
   
@@ -213,37 +275,109 @@ def session():
 @app.route('/records', methods=['GET'])
 @login_required
 def get_records():
+	data = request.get_json()
+	# Get the query parameters for filtering
+	date = data['date'] if 'date' in data else None
+	text = data['text'] if 'text' in data else None
+	calories_min = data['calories_min'] if 'calories_min' in data else None
+	calories_max = data['calories_max'] if 'calories_max' in data else None
+
+	try:
+		python_date = datetime.strptime(date, '%Y-%m-%d').date() if date else None
+	except:
+		abort(400)
+    
+	# Get the pagination parameters
+	try:
+		page = int(data['page']) if 'page' in data else None
+		limit = int(data['limit']) if 'limit' in data else None
+	except:
+		abort(400)
+
 	if current_user.role == 'user':
 		# For users with the 'user' role, retrieve their own records
 		user_id = current_user.id
-		entries = Entry.query.filter_by(user_id=user_id).all()
-		records = [{
+  
+		records, total_count = get_paginated_filtered_records(user_id, date, text, calories_min, calories_max, page, limit)
+
+		records_formatted = [{
 			'id': entry.id,
 			'text': entry.text,
 			'date': str(entry.date),
 			'time': str(entry.time),
 			'calories': entry.calories,
 			'is_below_expected': entry.is_below_expected
-		} for entry in entries]
-		return jsonify({ 'records': records })
+		} for entry in records]
+
+		return jsonify({
+			'records': records_formatted,
+			'total_count': total_count,
+			'page': page,
+			'limit': limit
+		})
+
 	elif current_user.role == 'admin':
-		# For users with the 'admin' role, retrieve records of all users
-		users = User.query.all()
-		dict = {}
-		for user in users:
-			if (user.role == 'manager'):
-				continue
-			entries = Entry.query.filter_by(user_id=user.id).all()
-			records = [{
+		user_id = data['user_id'] if 'user_id' in data else None
+		id = data['id'] if 'id' in data else None
+
+		if id:
+			entry = Entry.query.filter_by(id=id).first()
+			return jsonify({
+				'records': ({
+					'id': entry.id,
+					'text': entry.text,
+					'date': str(entry.date),
+					'time': str(entry.time),
+					'calories': entry.calories,
+					'is_below_expected': entry.is_below_expected
+				} if entry else {})
+			})
+
+		if user_id:
+			records, total_count = get_paginated_filtered_records(user_id, date, text, calories_min, calories_max, page, limit)
+
+			records_formatted = [{
 				'id': entry.id,
 				'text': entry.text,
 				'date': str(entry.date),
 				'time': str(entry.time),
 				'calories': entry.calories,
 				'is_below_expected': entry.is_below_expected
-			} for entry in entries]
-			dict[user.username] = records
-		return jsonify(dict)
+			} for entry in records]
+
+			return jsonify({
+				'records': records_formatted,
+				'total_count': total_count,
+				'page': page,
+				'limit': limit
+			})
+   
+		# For users with the 'admin' role, retrieve records of all users
+		users = User.query.all()
+		user_records = {}
+  
+		for user in users:
+			if (user.role == 'manager'):
+				continue
+   
+			records, total_count = get_paginated_filtered_records(user.id, date, text, calories_min, calories_max, page, limit)
+   
+			records_formatted = [{
+				'id': entry.id,
+				'text': entry.text,
+				'date': str(entry.date),
+				'time': str(entry.time),
+				'calories': entry.calories,
+				'is_below_expected': entry.is_below_expected,
+				'page': page,
+				'total_count': total_count,
+				'limit': limit
+			} for entry in records]
+			user_records[user.username] = records_formatted
+   
+		return jsonify({
+			'records': user_records
+		})
 	else:
 		# For unauthorized users, return a 403 Forbidden error
 		return abort(403)
@@ -254,9 +388,13 @@ def post_records():
 	user = current_user
 	if user.role == 'user' or user.role == 'admin':
 		data = request.get_json()
+		user_id = user.id
+		if user.role == 'admin':
+			if 'user_id' in data:
+				user_id = data['user_id']
 		if 'text' not in data:
 			abort(400)
-		if ('calories' not in data):
+		if 'calories' not in data:
 			# If user does not provide calories, we fetch it from Nutrionix API
 			food_name, calories = get_calories(data['text'])
 			if food_name == None or calories == None:
@@ -273,13 +411,14 @@ def post_records():
 		for record in Entry.query.filter_by(date=datetime.now().date()).all():
 			calories_sum += record.calories
 		calories_sum += int(data['calories'])
+		expected_calories = User.query.filter_by(id=user_id).first().expected_calories
 		entry = Entry (
-			user_id = user.id,
+			user_id = user_id,
 			date = datetime.now().date(),
 			time = datetime.now().time(),
 			text = data['text'],
 			calories = data['calories'],
-			is_below_expected = (calories_sum < user.expected_calories)
+			is_below_expected = (calories_sum < expected_calories)
 		)
 		db.session.add(entry)
 		db.session.commit()
@@ -289,7 +428,7 @@ def post_records():
 		})
 	else:
 		abort(403)
-  
+
 @app.route('/records', methods=['DELETE'])
 @login_required
 def delete_records():
@@ -336,7 +475,7 @@ def put_records():
 		if 'calories' in data:
 			record.calories = data['calories']
 		if 'text' in data:
-			record.calories = data['text']
+			record.text = data['text']
 		db.session.commit()
 		return jsonify({
 			'success': True,
