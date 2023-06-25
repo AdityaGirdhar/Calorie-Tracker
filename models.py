08caca4362e7d01dff0275e3d2baac36f53fc580
Aditya Girdhar <92713380+adityagirdhar@users.noreply.github.com>
2023-06-25 21:42:18 +0530

    Added unit tests for models.py

--- models.py
+++ models.py
@@ -6,48 +6,48 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(64), nullable=False)
-    expected_calories = db.Column(db.Integer, nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, nullable=False)
+	password_hash = db.Column(db.String(128), nullable=False)
+	role = db.Column(db.String(64), nullable=False)
+	expected_calories = db.Column(db.Integer, nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
 
 class Entry(db.Model):
-    def __init__(self, *args, **kwargs):
-        super(Entry, self).__init__(*args, **kwargs)
-        self.calculate_is_below_expected()
-    
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    time = db.Column(db.Time, nullable=False)
-    text = db.Column(db.String(256), nullable=False)
-    calories = db.Column(db.Integer, nullable=False)
-    is_below_expected = db.Column(db.Boolean)
+	def __init__(self, *args, **kwargs):
+		super(Entry, self).__init__(*args, **kwargs)
+		self.calculate_is_below_expected()
+	
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	date = db.Column(db.Date, nullable=False)
+	time = db.Column(db.Time, nullable=False)
+	text = db.Column(db.String(256), nullable=False)
+	calories = db.Column(db.Integer, nullable=False)
+	is_below_expected = db.Column(db.Boolean)
 
-    user = db.relationship('User', backref=db.backref('entries', lazy=True))
-    
-    def calculate_is_below_expected(self):
-        user = User.query.get(self.user_id)
-        
-        if user is not None:
-            total_calories = Entry.query \
-                .filter(Entry.user_id == self.user_id, Entry.date == self.date) \
-                .with_entities(db.func.sum(Entry.calories)).scalar()
-            if total_calories is None:
-                total_calories = 0
+	user = db.relationship('User', backref=db.backref('entries', lazy=True))
+	
+	def calculate_is_below_expected(self):
+		user = User.query.get(self.user_id) if self.user_id is not None else None
+		
+		if user is not None:
+			total_calories = Entry.query \
+				.filter(Entry.user_id == self.user_id, Entry.date == self.date) \
+				.with_entities(db.func.sum(Entry.calories)).scalar()
+			if total_calories is None:
+				total_calories = 0
 			# Add the calories of the current entry
-            total_calories += self.calories
+			total_calories += self.calories
 			# Compute whether total_calories is below the expected value
-            self.is_below_expected = total_calories < user.expected_calories
-        else:
-            self.is_below_expected = False
+			self.is_below_expected = total_calories < user.expected_calories
+		else:
+			self.is_below_expected = False
--- test_models.py
+++ test_models.py
@@ -1,35 +1,88 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, date, time
 from models import User, Entry
-from app import db
+from app import app, db
 
 class UserModelTestCase(unittest.TestCase):
-    def test_password_hashing(self):
-        # Create a User instance
-        user = User(
-            username='test',
-            role='test'
-        	)
-        password = 'sample_password'
+	def test_password_hashing(self):
+		# Create a User instance
+		user = User(
+			username='test',
+			role='test',
+			expected_calories=2000
+			)
+		password = 'sample_password'
+		
+		# Call the set_password method
+		user.set_password(password)
+		
+		# Assert the expected outcomes
+		self.assertTrue(user.check_password(password))
+		self.assertFalse(user.check_password('some_other_password'))
+	
+class TestBackReference(unittest.TestCase):
+	def setUp(self):
+    	# Step 1: Initialize a database instance
+		self.app = app.test_client()
+		self.app_context = app.app_context()
+		self.app_context.push()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
+		self.app_context.pop()
+
+	def test_back_reference(self):
+		# Step 2: Add a dummy user
+		user = User(username='test_user', role='user', expected_calories=2000)
+		user.set_password('sample_password')
+		# Step 3: Add a dummy entry
+		entry = Entry(user=user, date=datetime.now().date(), time=datetime.now().time(),
+					  text='Test entry', calories=100, is_below_expected=False)
+
+		with self.app_context:
+			# Add the user and entry to the session
+			db.session.add(user)
+			db.session.add(entry)
+			db.session.commit()
+
+			# Query the user from the database
+			user = User.query.filter_by(username='test_user').first()
+
+			# Check the back reference
+			self.assertIn(entry, user.entries)
+   
+class TestCalculateIsBelowExpected(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_calculate_is_below_expected(self):
+        # Create a new user with an expected calorie value
+        user = User(username='test_user', role='user', expected_calories=2000)
+        user.set_password('sample_password')
+        db.session.add(user)
         
-        # Call the set_password method
-        user.set_password(password)
+        user = User.query.filter_by(username='test_user').first()
+
+        # Create multiple entries for different dates with different calorie values
+        for i in range(3):
+            entry = Entry(user_id=user.id, date=date(2023, 6, 18), time=time(12, 0), text=f'Entry {i}', calories=1000*i)
+            db.session.add(entry)
         
-        # Assert the expected outcomes
-        self.assertTrue(user.check_password(password))
-        self.assertFalse(user.check_password('some_other_password'))
-    
-class EntryModelTestCase(unittest.TestCase):
-    def test_db_relationship(self):
-        # Create an Entry instance
-        entry = Entry(
-            text='sample_food',
-            user_id=1,
-            date=datetime.date(),
-            time=datetime.time()
-            calories=1500,
-            )
-        
+        entries = Entry.query.all()
+
+        # Iterate over the entries and assert the value of is_below_expected
+        for entry in entries:
+            self.assertEqual(entry.is_below_expected, entry.calories < user.expected_calories)
 
 if __name__ == '__main__':
 	unittest.main()
